b39d54f28e0b34c3bcbc21dd9f6ab6115adc361a
SohanAhmedsn <sohan.ahmedself@gmail.com>
2024-06-08 21:54:48 +0600

    edit file add

--- AEL.API/AEL.API/obj/Debug/net8.0/AEL.API.AssemblyInfo.cs
+++ AEL.API/AEL.API/obj/Debug/net8.0/AEL.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AEL.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be30df8d1e204a53f9cdbb86468379befa5055ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09856bf1b8147c5bb452c07dd3d9c2664cb04f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("AEL.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AEL.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ClientApps/AEL.Client/ClientApp/src/app/components/book/book-edit/book-edit.component.ts
+++ ClientApps/AEL.Client/ClientApp/src/app/components/book/book-edit/book-edit.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from 'src/app/models/data/book';
+import { BookServices } from 'src/app/services/data/book.service';
+import { NotifyService } from 'src/app/services/data/notify.service';
 
 @Component({
   selector: 'app-book-edit',
   templateUrl: './book-edit.component.html',
-  styleUrls: ['./book-edit.component.css']
+  styleUrls: ['./book-edit.component.css'],
 })
-export class BookEditComponent {
+export class BookEditComponent implements OnInit {
+  book: Book = {};
+  bookForm: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(300),
+    ]),
+    dueDate: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required]),
+  });
+  constructor(
+    private bookService: BookServices,
+    private notifySvc: NotifyService,
+    private actvatedRoute: ActivatedRoute
+  ) {}
 
+  get f() {
+    return this.bookForm.controls;
+  }
+  save() {
+    if (this.bookForm.invalid) return;
+    Object.assign(this.book, this.bookForm.value);
+    this.bookService.put(this.book).subscribe({
+      next: (r) => {
+        this.notifySvc.notify('Data updated successfully', 'DISMISS');
+      },
+      error: (err) => {
+        this.notifySvc.notify('Failed to update data', 'DISMISS');
+      },
+    });
+  }
+  ngOnInit(): void {
+    let id: number = this.actvatedRoute.snapshot.params['id'];
+    this.bookService.getById(id).subscribe({
+      next: (r) => {
+        this.book = r;
+        this.bookForm.patchValue(this.book);
+      },
+      error: (err) => {
+        console.log(err.message || err);
+        this.notifySvc.notify('Failed to load department', 'DISMISS');
+      },
+    });
+  }
 }
